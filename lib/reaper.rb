5abedc4e3b1dbe829338c65c520b99fc9bab424c
Konstantin Akhantev <saviorisdead@gmail.com>
2014-04-04 10:27:22 +0400

    changes in Grappler and Reaper

--- lib/reaper.rb
+++ lib/reaper.rb
@@ -10,22 +10,22 @@ class Reaper
   def reap
     selector_groups = @source.selectors.distinct(:group)
     @result = []
+    @hook = Grappler.new
+
     selector_groups.each do |group|
-      ids_set = Grappler.new(@source.selectors.active.ids_set.where(:group => group).first).grapple_all.uniq
+      @hook.charge(@source.selectors.active.ids_set.where(:group => group).first)
+      ids_set = @hook.grapple_all.uniq
       selectors = @source.selectors.active.data_fields.where(:group => group)
 
       ids_set.each do |entity_id|
-        tender = @source.tenders.find_or_create_by(id_by_source: entity_id)
-        tender.id_by_source = entity_id
+        code = @hook.charge(selectors.active.where(:value_type => :code_by_source).first, entity_id).grapple
+        tender = @source.tenders.find_or_create_by(code_by_source: code)
         
-        tender.title = Grappler.new(selectors.where(:value_type => :title).first, entity_id).grapple
-        tender.tender_form = Grappler.new(selectors.where(:value_type => :tender_form).first, entity_id).grapple
-        tender.customer_name = Grappler.new(selectors.where(:value_type => :customer_name).first, entity_id).grapple
-        tender.customer_adress = Grappler.new(selectors.where(:value_type => :customer_adress).first, entity_id).grapple
-        tender.code_by_source =  Grappler.new(selectors.where(:value_type => :code_by_source).first, entity_id).grapple
-        tender.start_at = Grappler.new(selectors.where(:value_type => :start_at).first, entity_id).grapple
-        tender.published_at = Grappler.new(selectors.where(:value_type => :published_at).first, entity_id).grapple
+        selectors.each do |selector|
+          tender[selector.value_type] = @hook.charge(selector, entity_id).grapple
+        end
 
+        tender.id_by_source = entity_id
         tender.source_link = @source.external_link_templates[group.to_s].gsub('$entity_id', entity_id)
         tender.group = group
 
