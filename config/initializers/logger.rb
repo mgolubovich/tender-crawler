180424b993e3514a7391f66b30b68bdf712ce9f0
Cane Lynch <saviorisdead@gmail.com>
2014-05-06 13:05:13 +0400

    restructured logging

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -1,7 +1,17 @@
 require 'logger'
 
-LOG_FILEPATH = 'log/parser.log'
+class ParserLog
+  LOG_FILEPATH = 'log/parser.log'
 
-# Start logging in given file, keeping 10 old logfiles, rotating if size og log file exceeds 1Gb
-logger = Logger.new(LOG_FILEPATH, 10, 100 * 1024 * 1024 * 1024)
-logger.level = Logger::WARN
+  # Start logging in given file, keeping 10 old logfiles, rotating if size og log file exceeds 1Gb
+  @logger = Logger.new(LOG_FILEPATH, 10, 100 * 1024 * 1024 * 1024)
+  @logger.level = Logger::WARN
+
+  def self.logger
+    @logger
+  end
+
+  def self.logger=(new_logger)
+    @logger = new_logger
+  end
+end
