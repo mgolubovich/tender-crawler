b1699b762381a76671ab63b63fe23d54d2e54aaf
Cane Lynch <saviorisdead@gmail.com>
2014-05-15 11:33:01 +0400

    trigger invoking algoritm

--- lib/trigger_invoker.rb
+++ lib/trigger_invoker.rb
@@ -1,5 +1,13 @@
 class TriggerInvoker
-  def initialize(source)
-    
+
+  def self.invoke(trigger,tender)
+    if Object.const_defined?(trigger["class_name"])
+      trigger_class = trigger["class_name"].constantize
+      tender = trigger_class.invoke(tender)
+      tender
+    else
+      puts 'Baaad'
+    end
   end
+
 end
