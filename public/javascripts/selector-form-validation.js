86d545990f8278ef4335174cd78e94cc8ea9c5d2
NorD <nord@estp.ru>
2014-06-03 15:00:34 +0400

    bug fix

---
+++ public/javascripts/selector-form-validation.js
@@ -0,0 +1,14 @@
+var button = $('.btn.btn-primary.btn-lg');
+var form = $('.form-horizontal');
+var fieldDiv = $('.col-md-5');
+var field = $('#selector_value');
+
+button.click(function (e) {
+        e.preventDefault();
+        if (field.val().length > 0)
+            form.submit();
+        else {
+            fieldDiv.addClass('has-error');
+            field.attr('placeholder', 'value_type забыл!')
+        }
+    }
