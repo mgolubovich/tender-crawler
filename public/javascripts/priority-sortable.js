a14f711b33f35e98c327d43ce2d2ea6de06c6e43
nordz0r <nordazavr@gmail.com>
2014-06-20 17:55:50 +0400

    fix

--- public/javascripts/priority-sortable.js
+++ public/javascripts/priority-sortable.js
@@ -6,32 +6,12 @@ $('.sorted_table').sortable({
   placeholder: '<tr class="placeholder"/>'
 })
 
-// Sortable column heads
-var oldIndex
-$('.sorted_head tr').sortable({
-  containerSelector: 'tr',
-  itemSelector: 'th',
-  placeholder: '<th class="placeholder"/>',
-  vertical: false,
-  onDragStart: function (item, group, _super) {
-    oldIndex = item.index()
-    item.appendTo(item.parent())
-    _super(item)
-  },
-  onDrop: function  (item, container, _super) {
-    var field,
-    newIndex = item.index()
-
-    if(newIndex != oldIndex)
-      item.closest('table').find('tbody tr').each(function (i, row) {
-        row = $(row)
-        field = row.children().eq(oldIndex)
-        if(newIndex)
-          field.before(row.children()[newIndex])
-        else
-          row.prepend(field)
-      })
-
-    _super(item)
-  }
-})
+var selectors = $('.movable');
+selectors.drop(function () {
+    var index = 0;
+    selectors.each(function () {
+        $(this).child('input').val(index);
+        $(this).child('.priority-value').html(index);
+        index++;
+    });
+});
