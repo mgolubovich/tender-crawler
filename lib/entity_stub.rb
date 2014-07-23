5f7f797afc4037b9b828faba70ba8b4f3c74ace3
Cane Lynch <saviorisdead@gmail.com>
2014-07-23 11:52:37 +0400

    done entity-stub

--- lib/entity_stub.rb
+++ lib/entity_stub.rb
@@ -1,21 +1,32 @@
-class EntityStub
+class EntityStub < Hash
 
   def initialize(data = {})
-    @data = data
+    self.merge! data
   end
 
   def insert(key, value)
-    @data[key.to_sym] = [] unless @data.has_key?(key.to_sym)
-    @data[key.to_sym] << value
+    self[key.to_sym] = [] unless self.has_key?(key.to_sym)
+    self[key.to_sym] << value
   end
 
-  def data
+  def attributes
     result = {}
-    @data.each_pair do |key, value|
-
+    self.each_pair do |key, values_set|
+      result[key.to_sym] = best_of(values_set)
     end
+    result
   end
 
   private
   
+  def best_of(values_set)
+    return values_set unless values_set.kind_of?(Array)
+    return values_set.first if values_set.count == 1
+
+    best = values_set.first
+    values_set.each do |value|
+      best = value unless value.nil? || (value.kind_of?(String) && value.empty?)
+    end
+    best
+  end
 end
