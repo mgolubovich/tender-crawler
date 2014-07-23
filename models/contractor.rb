fb380cd72ba9a51abca5397e4c3d2a5c55da17af
NorD <nord@estp.ru>
2014-07-23 13:33:14 +0400

    first com

--- models/contractor.rb
+++ models/contractor.rb
@@ -1,9 +1,16 @@
 class Contractor
   include Mongoid::Document
   include Mongoid::Timestamps
-  
+
   has_many :protocols
 
   field :name, type: String
+  field :short_name, type: String
   field :inn, type: String
+  field :address, type: String
+  field :mail_address, type: String
+  field :email, type: String
+  field :customer_name, type: String
+  field :customer_telephone, type: String
+  field :customer_fax, type: String
 end
