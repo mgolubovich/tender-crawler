fed23aee4f3964d7ddc95b6233c0a3d4f0dea1e6
Konstantin Akhantev <saviorisdead@gmail.com>
2014-07-30 10:39:57 +0400

    fixed date parsing

--- lib/grappler.rb
+++ lib/grappler.rb
@@ -58,7 +58,7 @@ class Grappler
 
   def apply_date_format(data)
     begin
-      data = DateTime.parse(data, @selector.date_format)
+      data = DateTime.strptime(data, @selector.date_format)
       data = data.change(:offset => "+0400") if data.offset == (0/1)
       data.to_time
     rescue Exception
