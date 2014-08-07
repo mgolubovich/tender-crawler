ef3d04d3b62ead7a2d0b784cabe7b1c56519e1bd
Cane Lynch <saviorisdead@gmail.com>
2014-08-07 15:26:15 +0400

    olol

--- controllers/cartridges_controller.rb
+++ controllers/cartridges_controller.rb
@@ -191,8 +191,7 @@ class CartridgesController < ApplicationController
     data = params[:data]
     data.each_pair do |id, priority|
       selector = Selector.find id
-      #debugger
-      selector.update_attributes!({'priority' => priority})
+      selector.update_attributes!('priority' => priority)
     end
     nil
   end
