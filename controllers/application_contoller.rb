75f83b38039fca57d47cfe7eb6fe48305f8c263f
Cane Lynch <saviorisdead@gmail.com>
2014-08-15 16:53:44 +0400

    small fixes

--- controllers/application_contoller.rb
+++ controllers/application_contoller.rb
@@ -1,3 +1,4 @@
+# Base controller, not used directly
 class ApplicationController < Sinatra::Base
   enable :sessions, :method_override
   set :views, File.expand_path('../../views', __FILE__)
@@ -5,14 +6,27 @@ class ApplicationController < Sinatra::Base
   configure :development do
     Bundler.setup(:default, :development)
     set :environment, :development
-    enable :sessions, :logging, :static, :inline_templates, :method_override, :dump_errors, :run
+    enable(
+      :sessions,
+      :logging,
+      :static,
+      :inline_templates,
+      :method_override,
+      :dump_errors, :run
+    )
     Mongoid.load!(File.expand_path(File.join('config', 'mongoid.yml')))
   end
 
   configure :test do
     Bundler.setup(:default, :test)
     set :environment, :test
-    enable :sessions, :static, :inline_templates, :method_override, :raise_errors
+    enable(
+      :sessions,
+      :static,
+      :inline_templates,
+      :method_override,
+      :raise_errors
+    )
     disable :run, :dump_errors, :logging
     Mongoid.load!(File.expand_path(File.join('config', 'mongoid.yml')))
   end
@@ -20,7 +34,15 @@ class ApplicationController < Sinatra::Base
   configure :production do
     Bundler.setup(:default, :production)
     set :environment, :production
-    enable :sessions, :logging, :static, :inline_templates, :method_override, :dump_errors, :run
+    enable(
+      :sessions,
+      :logging,
+      :static,
+      :inline_templates,
+      :method_override,
+      :dump_errors,
+      :run
+    )
     Mongoid.load!(File.expand_path(File.join('config', 'mongoid.yml')))
   end
 
